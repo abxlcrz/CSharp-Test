f15e6057f5fc905cc300b943398e20f5e72126c2
bboycien <47899906+bboycien@users.noreply.github.com>
2019-06-13 20:36:19 -0300

    ejercicio 1 resuelto
    muestra la suma de los numeros anteriores al que se ingresa por consola

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("ingrese un numero");
+            string numberInput= Console.ReadLine();
+            bool result= Int32.TryParse(numberInput, out int numberConverted); 
+                if (result==true){
+                    
+                    int suma =0;
+                    for (int i=0; i <numberConverted; i++) {
+                      int k=i;
+                      suma=suma+k;
+                     
+                    }
+                    Console.WriteLine("la suma total es "+suma);
+
+                }
+
+            
+        }
+    }
+}
